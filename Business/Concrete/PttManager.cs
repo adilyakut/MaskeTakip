640050ea77e38d8900e757265dff428d580db53f
Adil Yakut <70631241+adilyakut@users.noreply.github.com>
2024-02-08 22:29:46 +0100

    Initial commit

---
+++ Business/Concrete/PttManager.cs
@@ -0,0 +1,33 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class PttManager : ISupplierService
+    {
+        private IApplicantService _applicantService;
+
+        public PttManager(IApplicantService applicantService) 
+        {
+            _applicantService = applicantService;
+        }
+
+
+        public void GiveMask(Citizen citizen)
+        {
+            if(_applicantService.CheckCitizen(citizen))
+            {
+                Console.WriteLine(citizen.FirstName + " icin maske verildi.");
+            }
+            else
+            {
+                Console.WriteLine("maske verilemedi!");
+            }
+        }
+    }
+}
---
+++ Workaround/Program.cs
@@ -0,0 +1,54 @@
+﻿
+
+using Business.Concrete;
+using Entities.Concrete;
+
+SelamVer("Deli");
+Topla(4,2);
+
+
+//Arrays ...
+string [] students = new string[3];
+students[0] = "Deli";
+students[1] = "Veli";
+students[2] = "Zeki";
+
+foreach (string student in students)
+{
+    Console.WriteLine(student);
+}
+
+
+List<string> cities = new List<string> { "Ankara", "Istanbul","Izmir"};
+cities.Add("Adana");
+
+foreach (string city in cities)
+{
+    Console.WriteLine(city);
+}
+
+static void SelamVer(string name)
+{
+    Console.WriteLine("hello " + name);
+}
+
+
+static int Topla(int a, int b)
+{
+
+    int result = a + b;
+    Console.WriteLine(result);
+    return result;
+}
+
+Citizen citizen1 = new Citizen();   
+citizen1.FirstName  = "Engin";
+citizen1.LastName = "Demirog";
+citizen1.NationalIdentity = 123456789010;
+citizen1.DateOfBirthYear = 1985;
+
+
+
+PttManager pttManager = new PttManager(new CitizenManager());
+pttManager.GiveMask(citizen1);
+
